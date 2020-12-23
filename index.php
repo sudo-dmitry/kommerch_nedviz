a63808a64e433890f553d58e1edc36290083cfea
sudo-dmitry <dmitry.v.pletnev@gmail.com>
2020-12-23 17:37:30 +0300

    moved index

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="public/js/jquery-3.5.1.js"></script>
+    <link rel="stylesheet" type="text/css" href="public/slick/slick.css"/>
+    <link rel="stylesheet" href="public/css/style.min.css">
+    <title>Коммерческая недвижимость | Краснодар</title>
+</head>
+<body>
+
+
+
+<?php include 'public/components/navbar.php'; ?>
+
+<?php include 'public/components/hero.php'; ?>
+
+<?php include 'public/components/offer.php'; ?>
+
+<?php include 'public/components/objects.php'; ?>
+
+<?php include 'public/components/carousel.php'; ?>
+
+<?php include 'public/components/footer.php'; ?>
+
+
+
+
+
+<?php include 'public/components/svg.php'; ?>
+    <script type="text/javascript" src="public/js/lozad.js"></script>
+    <script type="text/javascript" src="public/js/app.min.js"></script>
+    <script type="text/javascript" src="public/slick/slick.min.js"></script>
+</body>
+</html>
