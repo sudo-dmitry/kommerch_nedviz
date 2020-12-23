6a887871f7181da3e0b7093d7f06e8d954cc4555
sudo-dmitry <dmitry.v.pletnev@gmail.com>
2020-12-23 17:43:11 +0300

    fixed paths to images in OBJECTS section

--- public/components/objects.php
+++ public/components/objects.php
@@ -3,16 +3,16 @@
         <div class="wrapper grid">
 
             <a href="" class="wrapper__accommodation">
-                <div class="item lazy" data-background-image="img/acco.jpg"></div>
+                <div class="item lazy" data-background-image="public/img/acco.jpg"></div>
             </a>
             <a href="" class="wrapper__business">
-                <div class="item lazy" data-background-image="img/business.jpg"></div>
+                <div class="item lazy" data-background-image="public/img/business.jpg"></div>
             </a>
             <a href="" class="wrapper__land">
-                <div class="item lazy" data-background-image="img/land.jpg"></div>
+                <div class="item lazy" data-background-image="public/img/land.jpg"></div>
             </a>
             <a href="" class="wrapper__industrial">
-                <div class="item _long lazy" data-background-image="img/industrial.jpg"></div>
+                <div class="item _long lazy" data-background-image="public/img/industrial.jpg"></div>
             </a>
 
 
