cc1624083e83a0e77fea326dc594010e8de0e27b
sudo-dmitry <dmitry.v.pletnev@gmail.com>
2021-01-11 18:29:38 +0300

    email send prod

--- sendemail.php
+++ sendemail.php
@@ -30,6 +30,7 @@ require 'vendor/autoload.php';
 echo 1;
 
 $from = new SendGrid\Email(null, "test@example.com");
+
 $subject = "Hello World from the SendGrid PHP Library!";
 $to = new SendGrid\Email(null, "dmitry.v.pletnev@gmail.com");
 $content = new SendGrid\Content("text/plain", "Hello, Email!");
@@ -37,6 +38,8 @@ $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('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');
 $sg = new \SendGrid($apiKey);
+
+?><pre><? print_r($sg) ?></pre><?php
 
 $response = $sg->client->mail()->send()->post($mail);
 
