1e4cd348ca7103d69fcdab162cbe74d131bd6339
sudo-dmitry <dmitry.v.pletnev@gmail.com>
2021-01-11 18:32:50 +0300

    email send prod

---
+++ vendor/composer/installed.php
@@ -0,0 +1,58 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bd0ca06d72d9ac914c108c7459f6f85001802ab8',
+    'name' => 'vendor_name/kommerch_nedviz',
+  ),
+  'versions' => 
+  array (
+    'sendgrid/php-http-client' => 
+    array (
+      'pretty_version' => '3.13.0',
+      'version' => '3.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '35862113b879274c7014e09681ac279a186665f1',
+    ),
+    'sendgrid/sendgrid' => 
+    array (
+      'pretty_version' => '7.9.1',
+      'version' => '7.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c3b76df9d2374b469474ab209fbd4e4ef1a16986',
+    ),
+    'sendgrid/sendgrid-php' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'starkbank/ecdsa' => 
+    array (
+      'pretty_version' => '0.0.4',
+      'version' => '0.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9369d35ed9019321adb4eb9fd3be21357d527c74',
+    ),
+    'vendor_name/kommerch_nedviz' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bd0ca06d72d9ac914c108c7459f6f85001802ab8',
+    ),
+  ),
+);
